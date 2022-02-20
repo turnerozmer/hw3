ec810f36b212bd172e595ede5634864a45c8069a
turnerozmer <turner.ozmer@kellogg.northwestern.edu>
2022-02-20 00:32:52 +0000

    merging again after adding some posts functiona

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,3 +1,21 @@
 class PostsController < ApplicationController
-    
+
+    def index
+        @posts = Post.all
+    end
+
+    def show
+        @post = Post.find(params["id"])
+    end
+
+    def new
+        @post = Post.new
+    end
+
+    def create
+        @post = Post.new(params["post"])
+        @post.save
+        redirect_to "/places/#{post.place.id}"
+    end
+
 end
